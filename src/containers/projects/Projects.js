6ee10f47ad06786e929d8cc3489adbd7c33fcebb
Ayush Munot <ayushmunot@Ayushs-MacBook-Pro.local>
2020-09-10 00:09:16 -0400

    changed github token

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -1,94 +1,98 @@
-import React, { useState, useEffect, lazy, Suspense } from "react";
-import ApolloClient from "apollo-boost";
-import { gql } from "apollo-boost";
-import "./Project.css";
-import Button from "../../components/button/Button";
-import Loading from "../loading/Loading";
-import { openSource, socialMediaLinks } from "../../portfolio";
+import React, { useState, useEffect } from "react"
+// import React, { useState, useEffect, Suspense } from "react"
+// import loadable from '@loadable/component'
+import ApolloClient from "apollo-boost"
+import { gql } from "apollo-boost"
+import { Fade } from "react-reveal"
 
+import "./Project.css"
+import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard"
+import Button from "../../components/button/Button"
+// import Loading from "../loading/Loading"
+import { openSource } from "../../portfolio"
 
 export default function Projects() {
-  const GithubRepoCard = lazy(() => import('../../components/githubRepoCard/GithubRepoCard'));
-  const FailedLoading = () => null ;
-  const renderLoader = () => <Loading />;
-  const [repo, setrepo] = useState([]);
+  
+  // const GithubRepoCard = loadable(() => import("../../components/githubRepoCard/GithubRepoCard"))
+  // const FailedLoading = () => null
+  // const RenderLoader = () => <Loading />
+  const [repo, setRepo] = useState([])
 
   useEffect(() => {
-    getRepoData();
-  }, []);
+    getRepoData()
+  }, [])
+  
+  function setRepoFunction(array) {
+    setRepo(array)
+  }
 
   function getRepoData() {
     const client = new ApolloClient({
       uri: "https://api.github.com/graphql",
-      request: (operation) => {
+      request: operation => {
         operation.setContext({
           headers: {
-            authorization: `Bearer ${openSource.githubConvertedToken}`,
-          },
-        });
-      },
-    });
+            authorization: `Bearer ${openSource.githubConvertedToken}`
+          }
+        })
+      }
+    })
 
     client
       .query({
         query: gql`
-        {
-        user(login: "${openSource.githubUserName}") {
-          pinnedItems(first: 6, types: [REPOSITORY]) {
-            totalCount
-            edges {
-              node {
-                ... on Repository {
-                  name
-                  description
-                  forkCount
-                  stargazers {
-                    totalCount
-                  }
-                  url
-                  id
-                  diskUsage
-                  primaryLanguage {
-                    name
-                    color
+          {
+            user(login: "${openSource.githubUserName}") {
+              pinnedItems(first: 6, types: [REPOSITORY]) {
+                totalCount
+                edges {
+                  node {
+                    ... on Repository {
+                      name
+                      description
+                      forkCount
+                      stargazers {
+                        totalCount
+                      }
+                      url
+                      id
+                      primaryLanguage {
+                        name
+                        color
+                      }
+                    }
                   }
                 }
               }
             }
           }
-        }
-      }
-        `,
+        `
       })
-      .then((result) => {
-        setrepoFunction(result.data.user.pinnedItems.edges);
-        console.log(result);
+      .then(result => {
+        setRepoFunction(result.data.user.pinnedItems.edges)
       })
-      .catch(function (error) {
-        console.log(error);
-        setrepoFunction("Error");
-        console.log("Because of this Error, nothing is shown in place of Projects section. Projects section not configured");
-      });
+      // .catch(function (error) {
+      //   setRepoFunction("Error")
+      // })
   }
-
-  function setrepoFunction(array) {
-    setrepo(array);
-  }
-  if (!(typeof repo === 'string' || repo instanceof String)){
+  
+  // if (!(typeof repo === 'string' || repo instanceof String)) {
   return (
-    <Suspense fallback={renderLoader()}>
-      <div className="main" id="opensource">
-        <h1 className="project-title">Some projects I have worked on . . . </h1>
+    // <Suspense fallback={RenderLoader()}>
+    <Fade bottom cascade duration={1000} distance="20px">
+      <div className="main" id="work">
+        <h1 className="project-title">Some Things I've Built</h1>
         <div className="repo-cards-div-main">
           {repo.map((v, i) => {
-            return <GithubRepoCard repo={v} key={v.node.id} />;
+            return <GithubRepoCard repo={v} key={v.node.id} />
           })}
         </div>
-        <Button text={"More Projects"} className="project-button" href={socialMediaLinks.github} newTab={true} />
+        <Button text={"More Projects"} className="project-button" href="https://github.com/pratikms" newTab={true} />
       </div>
-    </Suspense>
-  );
-} else{
-    return(<FailedLoading />);
-  }
+    </Fade>
+    // </Suspense>
+  )
+  // } else {
+  //   return(<FailedLoading />)
+  // }
 }
